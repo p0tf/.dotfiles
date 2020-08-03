f67abda9d5cad41b223e2d3bf04c4eb258496f85
Kanta Amano <kantaa058@gmail.com>
2020-08-03 13:35:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,20 @@
 set -e
 
 _yes_no() {
-    read -p "[WARN] File $1 exists. Overwrite? [y/N]: " ans
-    case $ans in
+    echo -n "$1 "
+    if [ x$2 = "xn" ];then
+        read -p "[y/N]: " ans
+        case $ans in
         [Yy]* ) return 0;;
         * ) return 1;;
-    esac
+        esac
+    else
+        read -p "[Y/n]: " ans
+        case $ans in
+        [Nn]* ) return 1;;
+        * ) return 0;;
+        esac
+    fi
 }
 
 _main() {
@@ -17,13 +26,17 @@ _main() {
     if [ ! -d $HOME/.dotfiles ];then
         cd $HOME
         echo [INFO] Main directory is missing. Now retrieving.
+        if which git; then
+            echo "[ERROR] \"git\" is missing. Please install."
+            return 1
+        fi
         git clone https://github.com/watcol/.dotfiles.git
     fi
 
     cd $HOME/.dotfiles/
 
     # Ignore denylist
-    for f in `find | grep -Pv '^./(.git|init|README.md|LICENSE|install.sh)'`;do
+    for f in `find | cut -c3- | grep -Pv '^($|.git|init|README.md|LICENSE|install.sh)'`;do
 
         if [ -d $f ] && [ ! -e $HOME/$f ];then
             mkdir $HOME/$f
@@ -31,17 +44,16 @@ _main() {
             if [ ! -e $HOME/$f ];then
                 ln -s $f $HOME/$f
                 echo [INFO] File \"$f\" is successfully linked.
-            elif _yes_no $HOME/$f;then
+            elif _yes_no "[WARN] File $HOME/$f exists. Overwrite?" n ;then
                 rm $HOME/$f
-                ln -s $f $HOME/$f
+                ln -s $HOME/.dotfiles/$f $HOME/$f
                 echo [INFO] $f is successfully linked.
             fi
         fi
     done
 
     for f in `ls -1 init/`;do
-        if [ -x $f ];then
-            echo [INFO] Run a configure script \"$f\".
+        if [ -x init/$f ] && _yes_no "[INFO] Do you want to run init/$f?" y ;then
             $f
         fi
     done
