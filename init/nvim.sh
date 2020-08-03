de044745789618ac7ce7620fc23b22571f976c61
Kanta Amano <kantaa058@gmail.com>
2020-08-03 13:51:08 +0900

    Add nvim

---
+++ init/nvim.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+_main() {
+    local $cur_dir `pwd`
+    if which git; then
+        echo [ERROR] \"git\" is missing. Please install.
+        return 1
+    elif which nvim; then
+        echo [ERROR] \"nvim\" is missing. Please install.
+        return 1
+    fi
+
+    mkdir -p $HOME/.dein/repos/github.com/Shougo/
+    cd $HOME/.dein/repos/github.com/Shougo/
+    git clone https://github.com/Shougo/dein.vim.git
+
+    cd $cur_dir
+    nvim --headless +"call dein#install()" +q
+}
+
+_main
